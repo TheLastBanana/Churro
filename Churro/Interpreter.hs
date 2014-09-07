b5716953145077b6fcf0cca883513e4d8a759ba7
Elliot Colp <thelastbanana@gmail.com>
2014-09-07 16:57:14 -0600

    Added loops
    Changed return types to a custom Churro return type to handle certain
    cases

--- Churro/Interpreter.hs
+++ Churro/Interpreter.hs
@@ -14,40 +14,48 @@ import qualified Data.Vector as V
 -}
 type ChurroState a = StateT ([Int], V.Vector Integer) IO a
 
+{-
+    Return type for churro parsing
+-}
+data ChurroReturn =
+      Continue  -- Interpreting should continue
+    | Complete  -- Stop interpreting (within current loop)
+    | Abort     -- Stop interpreting (passed fully up call chain)
+
 {--------------------------------- OPERATIONS ---------------------------------}
 
 {-
     Print a stack error
 -}
-stackError :: String -> ChurroState Bool
+stackError :: String -> ChurroState ChurroReturn
 stackError opName =
     do{ liftIO $ putStrLn ("Stack error: not enough values for "
                            ++ opName
-                           ++ " operation!")
-      ; return False
+                           ++ " operation")
+      ; return Abort
       }
 
 {-
     Push a number
 -}
-push :: Int -> ChurroState Bool
+push :: Int -> ChurroState ChurroReturn
 push n =
     do{ (stack, vec) <- get
       ; put (n:stack, vec)
-      ; return True
+      ; return Continue
       }
 
 {-
     Pop/peek A; discard A
 -}
-pop :: Bool -> ChurroState Bool
+pop :: Bool -> ChurroState ChurroReturn
 pop peek =
     do{ (stack, vec) <- get
       ; case stack of
             a:xs ->
                 do{ let newStack = if peek then stack else xs
                   ; put (newStack, vec)
-                  ; return True
+                  ; return Continue
                   }
             
             _    -> stackError "pop"
@@ -56,14 +64,14 @@ pop peek =
 {-
     Pop/peek A, B; push (B + A)
 -}
-add :: Bool -> ChurroState Bool
+add :: Bool -> ChurroState ChurroReturn
 add peek =
     do{ (stack, vec) <- get
       ; case stack of
             a:b:xs ->
                 do{ let newStack = if peek then stack else xs
                   ; put ((b + a):newStack, vec)
-                  ; return True
+                  ; return Continue
                   }
             
             _    -> stackError "add"
@@ -72,36 +80,83 @@ add peek =
 {-
     Pop/peek A, B; push (B - A)
 -}
-sub :: Bool -> ChurroState Bool
+sub :: Bool -> ChurroState ChurroReturn
 sub peek =
     do{ (stack, vec) <- get
       ; case stack of
             a:b:xs ->
                 do{ let newStack = if peek then stack else xs
                   ; put ((b - a):newStack, vec)
-                  ; return True
+                  ; return Continue
                   }
             
             _    -> stackError "subtract"
       }
 
 {-
-    Pop/peek A; if A = 0, exit loop
-    
-    At end of each iteration:
-    Pop/peek A; if A != 0, continue loop
+    Pop/peek A; if A = 0, jump to end of loop
 -}
-loop :: [ChurroOp] -> Bool -> Bool -> ChurroState Bool
-loop ops peekA peekB =
+startLoop :: [ChurroOp] -> Bool -> Bool -> ChurroState ChurroReturn
+startLoop ops peekA peekB =
     do{ (stack, vec) <- get
       ; case stack of
-            a:b:xs ->
+            a:xs ->
+                -- Pop from stack
                 do{ let newStack = if peekA then stack else xs
                   ; put (newStack, vec)
-                  ; interpret ops
+                  
+                  ; if a == 0
+                    -- Jump to end of loop
+                    then return Continue
+                    
+                    -- Start loop
+                    else do{ ret <- interpret ops
+                           ; case ret of
+                                 -- End of loop
+                                 Complete -> 
+                                     do{ ret <- endLoop peekB
+                                       ; case ret of
+                                             -- No error; continue loop
+                                             Continue -> startLoop ops peekA peekB
+                                        
+                                             -- No error; stop loop
+                                             Complete -> return Continue
+                                            
+                                             -- Error; stop everything
+                                             Abort -> return Abort
+                                        }
+                                 
+                                 -- Error
+                                 Abort -> return Abort
+                                 
+                                 -- This shouldn't happen
+                                 Continue ->
+                                     do{ liftIO $ putStrLn "Error: unexpected loop continuation"
+                                       ; return Abort
+                                       }
+                           }
                   }
             
             _    -> stackError "begin loop"
+      }
+
+{-
+    Pop/peek A; if A != 0, continue loop
+    Only called inside a loop
+-}
+endLoop :: Bool -> ChurroState ChurroReturn
+endLoop peek =
+    do{ (stack, vec) <- get
+      ; case stack of
+            a:xs ->
+                do{ let newStack = if peek then stack else xs
+                  ; put (newStack, vec)
+                  ; if a == 0
+                    then return Complete
+                    else return Continue
+                  }
+            
+            _    -> stackError "end loop"
       }
 
 {-------------------------------- INTERPRETER ---------------------------------}
@@ -109,31 +164,31 @@ loop ops peekA peekB =
 {-
     Execute the first operation and return whether to continue
 -}
-execOp :: ChurroOp -> ChurroState Bool
+execOp :: ChurroOp -> ChurroState ChurroReturn
 execOp op =
     do{ case op of
             Push n -> push n
             Pop peek -> pop peek
             Add peek -> add peek
             Sub peek -> sub peek
+            Loop ops peekA peekB -> startLoop ops peekA peekB
       }
 {-
     Interpret Churro code
 -}
-interpret :: [ChurroOp] -> ChurroState Bool
+interpret :: [ChurroOp] -> ChurroState ChurroReturn
 interpret (x:xs) =
     -- Execute operations
-    do{ continue <- execOp x
-      ; if continue
-        then interpret xs
-        else return False
+    do{ ret <- execOp x
+      ; (stack, _) <- get
+      ; liftIO $ putStrLn $ show stack
+      ; case ret of
+            Continue -> interpret xs
+            _        -> return ret
       }
 
 interpret _ =
-    do{ (stack, _) <- get
-      ; liftIO $ putStrLn $ show stack
-      ; return False
-      }
+    do{ return Complete }
 
 {-
     Parse and interpret Churro code
