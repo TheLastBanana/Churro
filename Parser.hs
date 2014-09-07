b068ea685ff57955d8b45260dac6fc3e119a0b87
Elliot Colp <thelastbanana@gmail.com>
2014-09-06 20:50:27 -0600

    Initial commit of parser and README

---
+++ Parser.hs
@@ -0,0 +1,247 @@
+module Churro.Parser
+    ( parseChurro
+    ) where
+
+import Text.Parsec
+import Text.Parsec.ByteString
+import Text.Parsec.Prim
+import Control.Monad
+import Data.Maybe
+import Data.Char
+import qualified Data.Sequence as S
+
+{--------------------------------- TYPES/DATA ---------------------------------}
+
+{-
+    Direction of churro facing (L = left, R = right)
+-}
+data Facing =
+      L
+    | R
+    deriving (Show, Eq)
+
+{-
+    Churro operations
+    Bools specifcy whether to peek (where appropriate)
+-}
+data ChurroOp =
+      Push Int
+    | Pop
+    | Add Bool
+    | Sub Bool
+    | Loop [ChurroOp] Bool Bool
+    | Store Bool
+    | Load Bool
+    | PrintInt Bool
+    | PrintChar Bool
+    | Read
+    | Exit
+    deriving (Show, Eq)
+
+{-
+    Parser for Churro code
+-}
+type ChurroParser a = Parsec [Char] [ChurroOp] a
+
+{-
+    Error or value
+-}
+type MaybeError a = Either String a
+    
+
+{----------------------------------- PARSER -----------------------------------}
+
+{-
+    Works like manyTill, but returns a tuple of
+    (values, endValue)
+    where values is the list of values returned by the parsers and endValue is
+    the values returned by the end parser.
+-}
+manyTillEnd :: (Stream s m t) => ParsecT s u m a -> ParsecT s u m end -> ParsecT s u m ([a], end)
+manyTillEnd p end = scan
+  where
+    scan =
+        do{ endr <- end; return ([], endr) }
+        <|>
+        do{ x <- p; (xs, endr) <- scan; return (x:xs, endr) }
+
+{-
+    Definition of whitespace
+-}
+whiteSpace = many $ oneOf " \n"
+    
+{-
+    Parse a churro filling
+-}
+filling :: ChurroParser Bool
+filling = 
+    do{ fill <- oneOf "o*" <?> "filling (\'*\' or \'o\')"
+      ; if fill == 'o'
+            then return False
+            else return True
+      }
+
+{-
+    Parse the head of a churro and return whether or not it is filled
+-}
+churroHead :: ChurroParser Bool
+churroHead = 
+    do{ char '{'
+      ; filled <- filling
+      ; char '}'
+      ; return filled
+      }
+      
+{-
+    Parse a churro's tail
+-}
+churroTail :: ChurroParser Int
+churroTail =
+    do{ tailStr <- many (char '=')
+      ; return $ length tailStr
+      }
+
+{-
+    Parse a left-facing (literal) churro
+-}
+lChurro :: ChurroParser (Bool, Int)
+lChurro = 
+    do{ filled <- churroHead
+      ; len <- churroTail
+      ; char '}'
+      ; whiteSpace
+      ; return (filled, len)
+      }
+
+{-
+    Parse a right-facing (literal) churro
+    Return (filled, tail length)
+-}
+rChurro :: ChurroParser (Bool, Int)
+rChurro = 
+    do{ char '{'
+      ; len <- churroTail
+      ; filled <- churroHead
+      ; whiteSpace
+      ; return (filled, len)
+      }
+
+{-
+    Parse any churro
+    Return (facing, filled, tail length)
+-}
+anyChurro :: ChurroParser (Facing, Bool, Int)
+anyChurro = 
+    -- Facing left
+    do{ (filled, len) <- try lChurro;
+      ; return (L, filled, len)
+      }
+    <|>
+    -- Facing right
+    do{ (filled, len) <- try rChurro;
+      ; return (R, filled, len)
+      }
+
+{-
+    Parse a specific type of churro
+    If Nothing is given as an argument, then that value is not checked
+    
+    Return (facing, filled, tail length)
+-}
+exactChurro :: Maybe Facing -> Maybe Bool -> Maybe Int
+               -> ChurroParser (Facing, Bool, Int)
+exactChurro facing filled len = 
+        -- Get a churro
+    do{ (pFacing, pFilled, pLen) <- anyChurro
+        -- Is it the right churro?
+      ; if foldl1 (&&) [(maybeEq facing pFacing),
+                        (maybeEq filled pFilled),
+                        (maybeEq len pLen)]
+        
+        -- Return its info
+        then return (pFacing, pFilled, pLen)
+        
+        -- Failed
+        else parserZero <?> "a churro with facing " ++ (show facing)
+                            ++ ", fill " ++ (show filled)
+                            ++ " and length " ++ (show len)
+      }
+  where
+    -- Nothing -> always match
+    -- Just a -> check a = b
+    maybeEq :: Eq a => Maybe a -> a -> Bool
+    maybeEq a b = maybe True (== b) a
+
+{-
+    Decode a single churro
+-}
+decodeChurro :: ChurroParser ChurroOp
+decodeChurro =
+    do{ (facing, filled, len) <- anyChurro
+      ; op <- case facing of
+            -- Literal
+            L -> do{ return $ Push (if filled then -len else len) }
+            
+            -- Operation
+            R -> case len of
+                0 -> do{ return Pop }
+                1 -> do{ return $ Add filled }
+                2 -> do{ return $ Sub filled }
+                
+                -- Decode a loop
+                3 -> do{ (ops, (_, endFilled, _)) <- churroLoop
+                       ; return $ Loop ops filled endFilled
+                       }
+                
+                -- This won't happen unless a start churro was left out
+                4 -> do{ unexpected "end churro" }
+                
+                5 -> do{ return $ Store filled }
+                6 -> do{ return $ Load filled }
+                7 -> do{ return $ PrintInt filled }
+                8 -> do{ return $ PrintChar filled }
+                9 -> do{ return Read }
+                10 -> do{ return Exit }
+      ; return op
+      }
+  where
+    -- Parse an end churro
+    endChurro = exactChurro (Just R) Nothing (Just 4)
+    
+    -- Parse until end of a loop
+    churroLoop = manyTillEnd decodeChurro (try endChurro)
+
+{-
+    Parse Churro code
+-}
+churroCode :: ChurroParser [ChurroOp]
+churroCode =
+    do{ whiteSpace
+      -- Find churros on this line
+      ; many $ do
+          { state <- getState
+          
+          -- Decode churro and add to operation list
+          ; op <- decodeChurro
+          ; setState $ op:state
+          }
+      ; state <- getState
+      ; return $ reverse state
+      }
+      
+  where
+    -- Try for an end churro
+    tryEnd = try $ exactChurro (Just R) Nothing (Just 4)
+  
+    -- Skip to next end churro
+    jumpAhead = 
+        do{ manyTill anyChurro tryEnd }
+
+{-
+    Run the churro parser
+-}
+parseChurro :: String -> Either ParseError [ChurroOp]
+parseChurro input = runParser churroCode [] "churro" input
+
+main :: IO ()
+main = putStrLn $ show $ parseChurro "{o}=} {==={o} {o}==} {===={*} {o}==}"
---
+++ README.md
@@ -0,0 +1,73 @@
+Churro
+======
+Churro is a stack-based interpreted programming language in which the code is
+entirely made up of churros.
+
+Design Principles
+-----------------
+* Churros are magical and should therefore be the only syntax element.
+* The language must include mutable state in order to irk Calvin Beck.
+
+Language Concepts
+-----------------
+Churro operates on two structures: a stack and an array. Operations can only be
+directly performed on data in the stack. The array is used to store data which
+may need to be recalled later. It is zero-indexed and contains 9999 elements,
+all of which are initialized to 0.
+
+Syntax
+------
+The only syntax elements are churros. A churro might look like this:
+`{o}=====}`
+
+There are three important components to a churro:
+
+1. Its orientation -- either left or right
+2. Its filling -- either 'o' (unfilled) or '*' (filled)
+3. Its tail length
+
+Literals
+--------
+Left-facing churros, also known as "literal churros," represent data. When a
+literal churro is encountered, its value is pushed to the top of the stack.
+
+The tail length of the churro -- that is, the number of "=" signs -- is its
+numerical value. Sign is indicated by a churro's filling, with unfilled churros
+(o) being positive and filled churros (*) being negative. All data is stored as
+32-bit integers.
+
+For example, this churro stands for 3:
+`{o}===}`
+
+this churro stands for -9:
+`{*}=========}`
+
+and this tiny one stands for 0:
+`{o}}`
+
+Operators
+---------
+Right-facing churros, also known as "operator churros," are used to manipulate
+data. They operate on values in the stack.
+
+The functions are:
+
+| Churro            | Operation                                                        |
+| ----------------- | ---------------------------------------------------------------- |
+| `{{o}`            | pop A; discard A                                                 |
+| `{={o}`           | pop A, B; push (A + B)                                           |
+| `{=={o}`          | pop A, B; push (A - B)                                           |
+| `{==={o}`         | pop A; if A = 0, jump to churro after next occurence of {===={o} |
+| `{===={o}`        | pop A; if A != 0, jump to churro after last occurence of {==={o} |
+| `{====={o}`       | pop A, B; store B in memory location A                           |
+| `{======{o}`      | pop A; push the value in memory location A to stack              |
+| `{======={o}`     | pop A; print A as an integer                                     |
+| `{========{o}`    | pop A; print A as an ASCII character                             |
+| `{========={o}`   | read a single character from stdin and push it to the stack      |
+| `{=========={o}`  | exit the program                                                 |
+
+Filled operator churros have the same behaviour as unfilled churros, but instead
+of popping values on the stack, they peek them.
+
+Finally, if an empty line is encountered, the program will exit, as there is no
+reason to exist without churros.
